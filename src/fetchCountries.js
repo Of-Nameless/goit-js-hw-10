76662492019a8aed6b6210b7e52d1c2abc5af536
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-02-13 19:06:42 +0100

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,4 @@
-// const BASE_URL = 'https://restcountries.com/v2';
-
-// export default function fetchCountries(name) {
-//     return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-//     .then(responce => {
-//         if (!responce.ok) {
-//             throw new Error(responce.status);
-//         }
-//         return responce.json()
-//     })
-// }
-
 export default function fetchCountries(searchQuery) {
-const URL = `https://restcountries.com/v3.1/name/${searchQuery}?fields=flags,name,capital,lang,population`;
+const URL = `https://restcountries.com/v3.1/name/${searchQuery}?fields=flags,name,capital,language,population`;
     return fetch(URL)       
 }
